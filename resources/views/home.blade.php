65e001cd6485206e303086c93b4b395be81fd16a
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-06-16 14:15:28 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('pageTitle', 'HOME')
+    
+
+@section('mainContent')
+<h1 class="text-center"><i class="fa-solid fa-home">Benvenuto</i></h1>
+@endsection
+
+
